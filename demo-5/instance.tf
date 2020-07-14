00822da078aeede8ddb5043956230d755c3a2744
Thet Min Ko <dev.minko@gmail.com>
2020-07-14 14:41:07 +0800

    Update

--- demo-5/instance.tf
+++ demo-5/instance.tf
@@ -1,19 +1,9 @@
-resource "aws_instance" "demo3" {
+resource "aws_instance" "demo5" {
   ami           = lookup(var.AWS_AMIS, var.AWS_REGION)
   instance_type = var.AWS_INSTANCE_TYPE
 
   tags = {
-    Name = "Demo 3"
-  }
-
-  # execute locally and write private ips of aws instance to a local file
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.demo3.private_ip} >> private_ips.txt"
+    Name = "Demo 5"
   }
 }
 
-# set ip variable using aws instance public ip value
-# output the variable
-output "ip" {
-  value = aws_instance.demo3.public_ip
-}
